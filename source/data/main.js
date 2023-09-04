bdda3c8e31c4449c2a0109abd631127451a74506
VanilaGrey <Allimp_94@mail.ru>
2023-09-04 05:17:46 +0300

    Начало  page-header

--- source/data/main.js
+++ source/data/main.js
@@ -1,4 +1,5 @@
 export default ({ error, pageName, status }) => ({
+	address: 'г. Москва, ул. Милашенкова, д.10',
 	appData: {
 		page: {
 			error,
@@ -6,7 +7,40 @@ export default ({ error, pageName, status }) => ({
 			status
 		}
 	},
-	description: 'My project',
-	pixelperfect: JSON.stringify({ breakpoints: [1200], ext: 'webp' }),
-	projectName: 'My app'
+	caption: 'Управление по развитию новых экономических форм производства',
+	description: 'Проект про судебные дела',
+	links: [
+		{
+			title: 'Главная',
+			url: '#!'
+		},
+		{
+			title: 'Наша команда',
+			url: '#!'
+		},
+		{
+			title: 'Контакты',
+			url: '#!'
+		}
+	],
+	pixelperfect: JSON.stringify({ breakpoints: [320], ext: 'webp' }),
+	projectName: 'Lawyers',
+	question: 'Есть вопросы? Мы онлайн',
+	socialLinks: [
+		{
+			id: 'telegram',
+			title: 'Мы в Телеграме',
+			url: '#!'
+		},
+		{
+			id: 'whats-app',
+			title: 'Мы в Whats app',
+			url: '#!'
+		}
+	],
+	tel: {
+		title: '+7 (495) 639-15-63',
+		url: 'tel:+74956391563'
+	},
+	webSite: 'urist.mos.help@yandex.ru'
 });
